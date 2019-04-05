c57ce5a3849f9da81f9b2d5213f67eb9b72d9963
liulangfeng <18270881162@163.com>
2019-04-06 00:38:19 +0800

    add project base setting

---
+++ myblog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Index(models.Model):
+    kind = models.CharField(
+        max_length=8,
+        primary_key=True,
+        default='others')
+
+class Blog(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=16)
+    kind = models.ForeignKey(
+        Index,
+        on_delete=models.CASCADE)
+    year = models.IntegerField()
+    mounth = models.IntegerField()
+    day = models.IntegerField()
+    text = models.OneToOneField(
+        Text,
+        on_delete=models.CASCADE)
+
+class Text(models.Model):
+    content = models.TextField()
+    
