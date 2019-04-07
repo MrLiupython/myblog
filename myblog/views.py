34afaf78b9ac3f0ff99b3a6a38dc249344300301
liulangfeng <18270881162@163.com>
2019-04-08 01:37:03 +0800

    add view function

---
+++ myblog/views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from .index import Index
+from .blog import Blog
+from markdown import markdown
+
+
+def  index_page(request):
+    indexs = Index.all()
+    return render(
+        request, 
+        'myblog/index.html',
+        {'indexs': indexs}
+        )
+
+
+def list_page(request):
+      if request.method == 'GET':
+         kind = request.GET.get('kind')
+         index_object = index.filter({'kind': kind})
+         blogs = index_object.blog_set.all()
+         return render(
+             request,
+             'myblog/list.html',
+             {'blogs': blogs}
+             )
+
+
+def blog_page(request):
+    blog_id = request.GET.get('_id')
+    if not blog_id:
+        return 'nothing'
+    blog = Blog.get(id=blog_id)
+    text = markdown(blog.text.text)
+    return render(
+        request,
+        'myblog/blog.html',
+        {'blog': blog, 'text': text}
+         
